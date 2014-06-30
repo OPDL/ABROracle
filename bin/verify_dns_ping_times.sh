c85f5bc3481e183822f82433fa8c7776cf509a0b
abrichards5 <abrichards5@gmail.com>
2014-06-30 17:09:23 -0600

    updates

--- bin/verify_dns_ping_times.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#from Oracle Sun Database Machine Setup/Configuration Best Practices [ID 1274318.1]
-#does not work on cygwin
-
-HOST_NAME=`hostname`; 
-DNS_SERVER=`nslookup $HOST_NAME | head -1 | cut -d: -f2 | sed -e 's/^[ \t]*//'`
-echo -e "Active DNS Server IP:\t\t$DNS_SERVER" 
-echo -n -e "Average for 10 pings in ms:\t"
- ping -c10 $DNS_SERVER | grep -E ^64| cut -d"=" -f 4 | cut -d" " -f 1 | awk '{ SUM += $1} END { print SUM/10}'
