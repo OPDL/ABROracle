a8324b4777bbbe7e23f05b76ea51a4cb253cc3ba
abrichards5 <abrichards5@gmail.com>
2014-08-21 17:06:24 -0600

    login audit queries

--- sql/last_login.sql
+++ sql/last_login.sql
@@ -1,15 +1,24 @@
-col username   format a15
-col last_login format a25
+set pagesize 9999
+set linesize 9999
+select username,'NEVER_LOGGED_IN' as "STATUS" from dba_users where account_status='OPEN' and username 
+not in ( select username from dba_audit_trail where action_name 
+in ('LOGOFF','LOGON') and username is not null );
 
-alter session set NLS_TIMESTAMP_TZ_FORMAT='DD.MM.YYYY HH24:MI:SS';
-
-Session altered.
-
-select 
-   username,
-   last_login 
-from
-   dba_users 
-order by 
-   username';
-
+select username,TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS') as "LAST_SUCCESSFUL_LOGIN" , returncode from
+(
+SELECT username, timestamp, returncode, max(TO_DATE (TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS'),'YYYY-MON-DD HH24:MI:SS')) over (partition by username) as max_ts
+from dba_audit_trail
+where action_name = 'LOGON'
+and returncode = 0
+) a
+where timestamp = a.max_ts
+order by username asc;
+select username,TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS') as "LAST_FAILED_LOGIN" , returncode from
+(
+SELECT username, timestamp, returncode, max(TO_DATE (TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS'),'YYYY-MON-DD HH24:MI:SS')) over (partition by username) as "MAX_TS"
+from dba_audit_trail
+where action_name = 'LOGON'
+and returncode > 0
+) a
+where timestamp = a."MAX_TS"
+order by username asc;
--- sql/last_login2.sql
+++
@@ -1,3 +0,0 @@
-sELECT t.*,
-       max( Last_Payment_Date ) OVER( partition by Account ) last_dat
-  FROM table1 t
