3769e7b4e1282a340e1b3458fdac662f108c7f21
abrichards5 <abrichards5@gmail.com>
2014-10-09 13:23:52 -0600

    Adam

---
+++ sql/reports/report_login_history.sql
@@ -0,0 +1,41 @@
+set pagesize 9999
+set linesize 9999
+set feedback off
+set verify off
+set colsep '|'
+DEFINE DAY_RANGE=7
+
+-- never logged in
+select username,'NEVER_LOGGED_IN' as "STATUS" from dba_users where account_status='OPEN' and username 
+not in ( select username from dba_audit_trail where action_name 
+in ('LOGOFF','LOGON') and username is not null );
+
+-- last successful login
+select USERNAME
+,TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS') as "LAST_SUCCESSFUL_LOGIN" 
+--,trunc((sysdate - TIMESTAMP)*24) "HOURS AGO"
+,trunc((sysdate - TIMESTAMP)) "DAYS AGO"
+from (
+select username,returncode, TIMESTAMP, row_number() over (partition by username order by username, timestamp desc) as "RN" from
+(
+SELECT username, timestamp, returncode, max(TO_DATE (TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS'),'YYYY-MON-DD HH24:MI:SS')) over (partition by username) as max_ts
+from dba_audit_trail
+where action_name = 'LOGON'
+and returncode = 0
+) a
+where timestamp = a.max_ts
+) b
+where b.rn = 1
+order by timestamp desc, username asc;
+
+-- last failed login
+select username,TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS') as "LAST_FAILED_LOGIN" , returncode from
+(
+SELECT username, timestamp, returncode, max(TO_DATE (TO_CHAR (TIMESTAMP, 'YYYY-MON-DD HH24:MI:SS'),'YYYY-MON-DD HH24:MI:SS')) over (partition by username) as "MAX_TS"
+from dba_audit_trail
+where action_name = 'LOGON'
+and returncode > 0
+) a
+where timestamp = a."MAX_TS"
+order by username asc;
+
