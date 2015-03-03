5038ffc6e113caeb7b1ffa18e36accdc9f2c9176
abrichards5 <abrichards5@gmail.com>
2015-03-03 14:01:25 -0700

    ldap

--- sql/custom/ldap_user/ldap_package.sql
+++ sql/custom/ldap_user/ldap_package.sql
@@ -1,28 +1,39 @@
--- LDAP Access Package
--- Author: Adam Richards
 CREATE OR REPLACE PACKAGE abrldap
+  -- LDAP Access Package
   -- Author: Adam Richards
 AS
   FUNCTION getUserGroups(
       sam VARCHAR2)
     RETURN INTEGER;
-  FUNCTION getUserGroups2(
-      sam VARCHAR2)
+  FUNCTION checkUserIsMemberOf(
+    sam VARCHAR2,
+    groupName varchar2
+    )
+    RETURN INTEGER;
+    FUNCTION getUserMemberOf(
+    sam VARCHAR2
+    )
     RETURN INTEGER;
 END abrldap;
 /
 CREATE OR REPLACE PACKAGE BODY abrldap
 IS
   -- Author: Adam Richards
+  -- private constants
+  LDAP_HOST    CONSTANT VARCHAR2(256) := 'mcp.epc.com';
+  LDAP_PORT    CONSTANT VARCHAR2(256) := '389';
+  LDAP_USER    CONSTANT VARCHAR2(256) := 'epc\AppAsrRBD';
+  LDAP_USER_PW CONSTANT VARCHAR2(256) := 'Nxr7WRB3';
+  LDAP_BASE    CONSTANT VARCHAR2(256) := 'dc=epc,dc=com';
 FUNCTION getUserGroups(
     sam VARCHAR2)
   RETURN INTEGER
 IS
-  l_ldap_host   VARCHAR2(256) := 'mcp.epc.com';
-  l_ldap_port   VARCHAR2(256) := '389';
-  l_ldap_user   VARCHAR2(256) := 'epc\AppAsrRBD';
-  l_ldap_passwd VARCHAR2(256) := 'Nxr7WRB3';
-  l_ldap_base   VARCHAR2(256) := 'dc=epc,dc=com';
+  l_ldap_host   VARCHAR2(256) := LDAP_HOST;
+  l_ldap_port   VARCHAR2(256) := LDAP_PORT;
+  l_ldap_user   VARCHAR2(256) := LDAP_USER;
+  l_ldap_passwd VARCHAR2(256) := LDAP_USER_PW;
+  l_ldap_base   VARCHAR2(256) := LDAP_BASE;
   l_retval PLS_INTEGER;
   l_session DBMS_LDAP.session;
   l_attrs DBMS_LDAP.string_collection;
@@ -64,15 +75,17 @@ BEGIN
   l_retval := DBMS_LDAP.unbind_s(ld => l_session);
   RETURN 0;
 END getUserGroups;
-FUNCTION getUserGroups2(
-    sam VARCHAR2)
+FUNCTION checkUserIsMemberOf(
+    sam VARCHAR2,
+    groupName varchar2
+    )
   RETURN INTEGER
 IS
-  ldap_host VARCHAR2(256);
-  ldap_port PLS_INTEGER;
-  ldap_user   VARCHAR2(256);
-  ldap_passwd VARCHAR2(256);
-  ldap_base   VARCHAR2(256);
+  l_ldap_host   VARCHAR2(256) := LDAP_HOST;
+  l_ldap_port   VARCHAR2(256) := LDAP_PORT;
+  l_ldap_user   VARCHAR2(256) := LDAP_USER;
+  l_ldap_passwd VARCHAR2(256) := LDAP_USER_PW;
+  l_ldap_base   VARCHAR2(256) := LDAP_BASE;
   retval PLS_INTEGER;
   my_session DBMS_LDAP.session;
   my_pset_coll DBMS_LDAP_UTL.PROPERTY_SET_COLLECTION;
@@ -85,21 +98,16 @@ IS
   group_type PLS_INTEGER;
   group_handle DBMS_LDAP_UTL.HANDLE;
 BEGIN
-  ldap_host   := 'mcp.epc.com';
-  ldap_port   := 389;
-  ldap_user   := 'epc\';
-  ldap_passwd := '';
-  ldap_base   := 'dc=epc,dc=com';
-  user_type   := DBMS_LDAP_UTL.TYPE_DN;
-  group_type  := DBMS_LDAP_UTL.TYPE_DN;
-  user_id     := 'user_or_group_dn';
-  group_id    := 'group_dn';
+  user_type  := DBMS_LDAP_UTL.TYPE_DN;
+  group_type := DBMS_LDAP_UTL.TYPE_DN;
+  user_id    := sam;
+  group_id   := 'group_dn';
   -- Choosing exceptions to be raised by DBMS_LDAP library.
   DBMS_LDAP.USE_EXCEPTION := TRUE;
-  my_session := DBMS_LDAP.init(ldap_host,ldap_port);
-  retval := DBMS_LDAP.simple_bind_s(my_session, ldap_user, ldap_passwd);
-  retval    := DBMS_LDAP_UTL.create_user_handle(user_handle,user_type,user_id);
-  IF retval != DBMS_LDAP_UTL.SUCCESS THEN
+  my_session              := DBMS_LDAP.init(l_ldap_host,l_ldap_port);
+  retval                  := DBMS_LDAP.simple_bind_s(my_session, l_ldap_user, l_ldap_passwd);
+  retval                  := DBMS_LDAP_UTL.create_user_handle(user_handle,user_type,user_id);
+  IF retval               != DBMS_LDAP_UTL.SUCCESS THEN
     -- Handle Errors
     DBMS_OUTPUT.PUT_LINE('create_user_handle returns : ' || TO_CHAR(retval));
   END IF;
@@ -124,25 +132,82 @@ BEGIN
   ELSE
     DBMS_OUTPUT.PUT_LINE('get_group_membership returns : Member');
   END IF;
-  
   DBMS_LDAP_UTL.free_handle(user_handle);
   DBMS_LDAP_UTL.free_handle(group_handle);
-  
   retval    := DBMS_LDAP.unbind_s(my_session);
   IF retval != DBMS_LDAP_UTL.SUCCESS THEN
     -- Handle Errors
     DBMS_OUTPUT.PUT_LINE('unbind_s returns : ' || TO_CHAR(retval));
   END IF;
+  RETURN 0;
   -- Handle Exceptions
 EXCEPTION
 WHEN OTHERS THEN
   DBMS_OUTPUT.PUT_LINE(' Error code : ' || TO_CHAR(SQLCODE));
   DBMS_OUTPUT.PUT_LINE(' Error Message : ' || SQLERRM);
   DBMS_OUTPUT.PUT_LINE(' Exception encountered .. exiting');
-END getUserGroups2;
+  RETURN 1;
+END checkUserIsMemberOf;
+
+FUNCTION getUserMemberOf(
+    sam VARCHAR2
+    )
+  RETURN INTEGER
+IS
+  l_ldap_host   VARCHAR2(256) := LDAP_HOST;
+  l_ldap_port   VARCHAR2(256) := LDAP_PORT;
+  l_ldap_user   VARCHAR2(256) := LDAP_USER;
+  l_ldap_passwd VARCHAR2(256) := LDAP_USER_PW;
+  l_ldap_base   VARCHAR2(256) := LDAP_BASE;
+  
+  retval PLS_INTEGER;
+  l_session DBMS_LDAP.session;
+  groups_prop_col DBMS_LDAP_UTL.PROPERTY_SET_COLLECTION;
+  l_attrs         DBMS_LDAP.string_collection;
+  my_property_names DBMS_LDAP.STRING_COLLECTION;
+  my_property_values DBMS_LDAP.STRING_COLLECTION;
+  user_handle DBMS_LDAP_UTL.HANDLE;
+  user_type PLS_INTEGER := DBMS_LDAP_UTL.TYPE_DN;
+BEGIN
+  DBMS_LDAP.USE_EXCEPTION := TRUE;
+  l_session               := DBMS_LDAP.init(l_ldap_host,l_ldap_port);
+  retval                  := DBMS_LDAP.simple_bind_s(l_session, l_ldap_user, l_ldap_passwd);
+  retval                  := DBMS_LDAP_UTL.create_user_handle(user_handle,user_type,sam);
+  IF retval != DBMS_LDAP_UTL.SUCCESS THEN
+    -- Handle Errors
+    DBMS_OUTPUT.PUT_LINE('create_user_handle returns : ' || TO_CHAR(retval));
+  END IF;
+  
+  l_attrs(1) := 'memberOf';
+  retval := DBMS_LDAP_UTL.get_group_membership(
+  ld => l_session,
+      user_handle => user_handle,
+      nested => DBMS_LDAP_UTL.NESTED_MEMBERSHIP,
+      attr_list => l_attrs,
+      ret_groups => groups_prop_col
+);
+  DBMS_LDAP_UTL.free_handle(user_handle);
+  retval    := DBMS_LDAP.unbind_s(l_session);
+  IF retval != DBMS_LDAP_UTL.SUCCESS THEN
+    DBMS_OUTPUT.PUT_LINE('unbind_s returns : ' || TO_CHAR(retval));
+  END IF;
+  RETURN 0;
+  -- Handle Exceptions
+EXCEPTION
+WHEN OTHERS THEN
+  DBMS_OUTPUT.PUT_LINE(' Error code : ' || TO_CHAR(SQLCODE));
+  DBMS_OUTPUT.PUT_LINE(' Error Message : ' || SQLERRM);
+  DBMS_OUTPUT.PUT_LINE(' Exception encountered .. exiting');
+  RETURN 1;
+END getUserMemberOf;
+
 END abrldap;
 /
 -- TEST
 SET serveroutput ON
 SELECT abrldap.getUserGroups('dsvrichards') FROM dual;
+/
+SELECT abrldap.checkUserIsMemberOf('dsvrichards','ITS') FROM dual;
+/
+SELECT abrldap.getUserMemberOf('dsvrichards') FROM dual;
 /
