de0981e7f4d47be6c746ca5f63954ab965402378
abrichards5 <abrichards5@gmail.com>
2015-05-06 15:28:21 -0600

    clean

--- bin/ldap_find_encoded.sh
+++ bin/ldap_find_encoded.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Enter your EPC username (ex: dsvsmith ): " UN
+read -p "Enter your LDAP username (ex: smith ): " UN
 printf "Enter LDAP Password: "
 ldapsearch -E pr=1000/noprompt -h mcp.epc.com -p 389 -D "${UN}@epc.com" -x -W -b "dc=epc,dc=com" "(&(objectclass=user)(!(objectclass=computer))(!(userAccountControl:1.2.840.113556.1.4.803:=2))(employeeID=*))" DN givenName sn sAMAccountName sAMAccountType displayName employeeID | grep -i -e 'displayName\|givenName\|sn\|employeeid\|samaccountname' > /tmp/xreflist
 
